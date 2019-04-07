661a4f578fd9d36098e4a2ac5f342adbef9b808c
capehill <juha.niemimaki@gmail.com>
2019-04-07 09:21:12 +0300

    Add command-line support

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # glsnoop
 An experimental graphics API tracing tool for AmigaOS 4
+
+## Tracing support
+
+- ogles2.library
+- Warp3DNova.library
+
+## Command-line parameters
+
+- OGLES2: enable ogles2.library tracing
+- NOVA: enable Warp3DNova.library tracing
+
+## License
+
+PUBLIC DOMAIN
+
--- main.c
+++ main.c
@@ -2,26 +2,68 @@
 #include "warp3dnova_module.h"
 
 #include <proto/exec.h>
-#include <dos/dos.h>
+#include <proto/dos.h>
 
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+struct Params {
+    LONG ogles2;
+    LONG nova;
+};
 
+static struct Params params = { FALSE };
+
+void parse_args()
+{
+    struct RDArgs *result = IDOS->ReadArgs("OGLES2/S,NOVA/S", (int32 *)&params, NULL);
+
+    if (result) {
+        IDOS->FreeArgs(result);
+    }
+
+    if (!params.ogles2 && !params.nova) {
+        // If user gives nothing, assume everything
+        params.ogles2 = params.nova = TRUE;
+    }
+
+    printf("OGLES2 tracing: %s\n", params.ogles2 ? "enabled" : "disabled");
+    printf("WARP3DNOVA tracing: %s\n", params.nova ? "enabled" : "disabled");
+}
+
+static void install_patches() {
+    if (params.ogles2) {
+        ogles2_install_patches();
+    }
+
+    if (params.nova) {
+        warp3dnova_install_patches();
+    }
+}
+
+static void remove_patches() {
+    if (params.nova) {
+        warp3dnova_remove_patches();
+    }
+
+    if (params.ogles2) {
+        ogles2_remove_patches();
+    }
+}
+
+int main(int argc, char* argv[])
+{
     IExec->DebugPrintF("glSnoop starting\n");
 
-    ogles2_install_patches();
-    warp3dnova_install_patches();
+    parse_args();
+    install_patches();
 
-    //IExec->DebugPrintF("Control-C to quit\n");
     printf("System patched. Press Control-C to quit...\n");
 
     IExec->Wait(SIGBREAKF_CTRL_C);
 
-    warp3dnova_remove_patches();
-    ogles2_remove_patches();
+    remove_patches();
 
-    IExec->DebugPrintF("glSnoop quiting\n");
+    IExec->DebugPrintF("glSnoop terminated\n");
 
     return 0;
 }
